4273825941c26e73685ba9d0d6f52833a08bfe57
Maramata Diop <maramatad@gmail.com>
2025-03-06 13:25:19 +0000

    Ajout des fichiers pour un bon déploiement avec Railway

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 echo "Démarrage du serveur Flask..."
-flask db upgrade  # Appliquer les migrations
-exec gunicorn -w 4 -b 0.0.0.0:5001 app:app  # Lancer le serveur avec Gunicorn
+
+# Attendre que la base de données soit prête (optionnel, mais recommandé)
+until pg_isready -h switchyard.proxy.rlwy.net -p 54807 -U postgres; do
+  echo "En attente de la base de données..."
+  sleep 2
+done
+
+# Appliquer les migrations avant le démarrage du serveur
+flask db upgrade  
+
+# Lancer Gunicorn pour exécuter l'application Flask
+exec gunicorn -w 4 -b 0.0.0.0:5001 app:app
